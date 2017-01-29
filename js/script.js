c5e27cf05c9637fed8be9fa5666a4eccbad0ae96
Sereda Vadim <flash.brovary@gmail.com>
2017-01-29 22:20:07 +0200

    fixed mobile version

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
+$(document).ready(function(){
+    $(".sticky-container").sticky({topSpacing:0});
+});
 
+//mobile pushy menu - button
+$('.menu-btn, .site-overlay').click(function(){
+    var menuBtn = $('.menu-btn');
 
+    if (menuBtn.hasClass('open')) {
+        menuBtn.removeClass('open');
+    } else {
+        menuBtn.toggleClass('open');
+    }
+});
